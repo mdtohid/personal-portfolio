6022540d711466cbfda6659cea224136e265ccd6
weeroDigital <132737228+weeroTheme2@users.noreply.github.com>
2024-04-27 20:34:13 +0600

    something wrong'

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import Contact from '../Contact/Contact';
-import Intro from '../Intro/Intro';
-import Projects from '../Projects/Projects';
-import Skills from '../Skills/Skills';
+import React from "react";
+import Contact from "../Contact/Contact";
+import Intro from "../Intro/Intro";
+import Projects from "../Projects/Projects";
+import Skills from "../Skills/Skills";
 
 const Home = () => {
-    return (
-        <div className='px-2 md:px-8 lg:px-28'>
-            <Intro></Intro>
-            <Skills></Skills>
-            <Projects></Projects>
-            <Contact></Contact>
-        </div>
-    );
+  return (
+    <div className="px-2 md:px-8 lg:px-28">
+      <Intro></Intro>
+      <Skills></Skills>
+      <Projects></Projects>
+      <Contact></Contact>
+    </div>
+  );
 };
-
+// dsfsdf
 export default Home;
