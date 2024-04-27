4eb0243e81d750395c43e32ac9d8c92023a3722e
weeroDigital <132737228+weeroTheme2@users.noreply.github.com>
2024-04-27 19:25:40 +0600

    Intro animation update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './Components/Header/Header';
-import Home from './Components/Home/Home';
-import { ToastContainer } from 'react-toastify';
-import Footer from './Components/Footer/Footer';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Components/Header/Header";
+import Home from "./Components/Home/Home";
+import { ToastContainer } from "react-toastify";
+import Footer from "./Components/Footer/Footer";
+import SkillDisplay from "./Components/SkillDisplay/SkillDisplay";
 
 function App() {
   return (
     <div className="App font-light bg-[#0e1630] text-[#808dad]">
       <Header></Header>
       <Home></Home>
-      <ToastContainer/>
+      <SkillDisplay />
+      <ToastContainer />
       <Footer></Footer>
     </div>
   );
